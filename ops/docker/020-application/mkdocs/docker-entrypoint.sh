d281304e1ca939693aac9943a63d2c2abb5eee19
Ryan Grippeling <R.Grippeling@hotmail.com>
2025-03-09 20:51:08 +0100

    feat!: moved out the runners

--- ops/docker/020-application/mkdocs/docker-entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-mkdocs build \
-  --clean \
-  --site-dir /tmp/techdocs-site
--- ops/docker/020-application/techdocs/docker-entrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-techdocs-cli generate \
-    --no-docker \
-    --verbose \
-    --source-dir /docs \
-    --output-dir /tmp/techdocs-site
